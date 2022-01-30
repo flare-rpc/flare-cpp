3beef6a6ce88c95b1839ffade473c6120b290efc
lijippy@163.com <lijippy@163.com>
2022-01-30 16:16:01 +0800

    fix testingA

--- flare/base/logging.h
+++ flare/base/logging.h
@@ -18,9 +18,9 @@ DECLARE_bool(crash_on_fatal_log);
 // define macros that not implemented in glog
 # ifndef DCHECK_IS_ON   // glog didn't define DCHECK_IS_ON in older version
 #  if defined(NDEBUG)
-#    define DCHECK_IS_ON()  FLAGS_crash_on_fatal_log
+#    define DCHECK_IS_ON()  0
 #  else
-#    define DCHECK_IS_ON() FLAGS_crash_on_fatal_log
+#    define DCHECK_IS_ON() 1
 #  endif  // NDEBUG
 # endif // DCHECK_IS_ON
 # if DCHECK_IS_ON()
