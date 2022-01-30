c0f3b246d11427cb0c5b20349f7dcafb583817a2
lijippy@163.com <lijippy@163.com>
2022-01-30 10:55:03 +0800

    fix

--- flare/strings/numbers.h
+++ flare/strings/numbers.h
@@ -218,7 +218,7 @@ namespace flare::strings {
         char *out
         ) {
 #ifdef __SSE4_2__
-        uint64_t be = big_endian::from_host64(val);
+        uint64_t be = flare::base::big_endian::from_host64(val);
         const auto kNibbleMask = _mm_set1_epi8(0xf);
         const auto kHexDigits = _mm_setr_epi8('0', '1', '2', '3', '4', '5', '6', '7',
                                               '8', '9', 'a', 'b', 'c', 'd', 'e', 'f');
