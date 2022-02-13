e77dbbeb7dc92a99297241dce891e447e332b201
lijippy@163.com <lijippy@163.com>
2022-02-13 22:22:31 +0800

    change log

--- test/bthread/bthread_mutex_unittest.cpp
+++ test/bthread/bthread_mutex_unittest.cpp
@@ -15,6 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
+#include <inttypes.h>
 #include <gtest/gtest.h>
 #include "flare/base/compat.h"
 #include "flare/base/time.h"
