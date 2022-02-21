02422f0cc9551b224a0106cb34f2e0fd8a945e92
lijippy@163.com <lijippy@163.com>
2022-02-21 14:51:24 +0800

    fiber

--- flare/fiber/runtime.cc
+++ flare/fiber/runtime.cc
@@ -7,11 +7,11 @@
 namespace flare::fiber {
 
     int fiber_getconcurrency(void) {
-        return flare::fiber_internal::fiber_getconcurrency();
+        return ::fiber_getconcurrency();
     }
 
     int fiber_setconcurrency(int num) {
-        return flare::fiber_internal::fiber_setconcurrency(num);
+        return ::fiber_setconcurrency(num);
     }
 
 }  // namespace flare::fiber
