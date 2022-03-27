d7b93b543cfbadda1fbadd91d7800736c88a710d
lijippy@163.com <lijippy@163.com>
2022-03-28 07:20:24 +0800

    format

--- flare/base/uninitialized.h
+++ flare/base/uninitialized.h
@@ -24,34 +24,34 @@ namespace flare::base {
         static void resize(string_type *s, size_t new_size) { s->resize(new_size); }
     };
 
-// __resize_default_init is provided by libc++ >= 8.0
+    // __resize_default_init is provided by libc++ >= 8.0
     template<typename string_type>
     struct resize_uninitialized_traits<
-            string_type, std::void_t < decltype(std::declval<string_type &>()
+            string_type, std::void_t<decltype(std::declval<string_type &>()
                     .__resize_default_init(237))> > {
-    using HasMember = std::true_type;
+        using HasMember = std::true_type;
 
-    static void resize(string_type *s, size_t new_size) {
-        s->__resize_default_init(new_size);
+        static void resize(string_type *s, size_t new_size) {
+            s->__resize_default_init(new_size);
+        }
+    };
+
+    // Returns true if the std::string implementation supports a resize where
+    // the new characters added to the std::string are left untouched.
+    //
+    template<typename string_type>
+    FLARE_FORCE_INLINE constexpr bool string_supports_uninitialized_resize(string_type *) {
+        return resize_uninitialized_traits<string_type>::HasMember::value;
     }
-};
 
-// Returns true if the std::string implementation supports a resize where
-// the new characters added to the std::string are left untouched.
-//
-template<typename string_type>
-FLARE_FORCE_INLINE constexpr bool string_supports_uninitialized_resize(string_type *) {
-    return resize_uninitialized_traits<string_type>::HasMember::value;
-}
-
-// Like str->resize(new_size), except any new characters added to "*str" as a
-// result of resizing may be left uninitialized, rather than being filled with
-// '0' bytes. Typically used when code is then going to overwrite the backing
-// store of the std::string with known data.
-template<typename string_type, typename = void>
-FLARE_FORCE_INLINE void string_resize_uninitialized(string_type *s, size_t new_size) {
-    resize_uninitialized_traits<string_type>::resize(s, new_size);
-}
+    // Like str->resize(new_size), except any new characters added to "*str" as a
+    // result of resizing may be left uninitialized, rather than being filled with
+    // '0' bytes. Typically used when code is then going to overwrite the backing
+    // store of the std::string with known data.
+    template<typename string_type, typename = void>
+    FLARE_FORCE_INLINE void string_resize_uninitialized(string_type *s, size_t new_size) {
+        resize_uninitialized_traits<string_type>::resize(s, new_size);
+    }
 
 
 }  // namespace flare::base
