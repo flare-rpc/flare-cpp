341ca89d2ca7710044738532d89da4ab90f8bef0
lijippy@163.com <lijippy@163.com>
2022-04-17 23:19:19 +0800

    add metrics

--- flare/metrics/expose_metrics.h
+++ flare/metrics/expose_metrics.h
@@ -5,12 +5,37 @@
 #ifndef FLARE_METRICS_EXPOSE_METRICS_H_
 #define FLARE_METRICS_EXPOSE_METRICS_H_
 
+#include <string>
+#include <ostream>
+#include <unordered_map>
+
 namespace flare {
 
     class expose_metrics {
     public:
+        typedef std::unordered_map<std::string, std::string> tag_type;
+    public:
+        expose_metrics() = default;
+
+        virtual ~expose_metrics() = default;
+
+        const std::string &prefix() const {
+            return _prefix;
+        }
+
+        const tag_type &tags() const {
+            return _tags;
+        }
+
+        bool expose();
+
+        bool hide();
+
+        virtual void dump(std::ostream &out) const;
 
     private:
+        std::unordered_map<std::string, std::string> _tags;
+        std::string _prefix;
     };
 }  // namespace flare
 
