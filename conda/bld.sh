07bcf7dbb018379745bf98100c084653494e8fb6
lijippy@163.com <lijippy@163.com>
2022-07-04 14:44:11 +0800

    fix test

--- conda/bld.sh
+++ conda/bld.sh
@@ -3,6 +3,6 @@ set -e
 
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release -DBUILD_UNIT_TESTS=OFF -DBUILD_EXAMPLES=OFF
 cmake --build .
 cmake --build . --target install
